838809c2492e5598ce7632863f07a2f1339350aa
stormyyy <100660727+StormLight14@users.noreply.github.com>
2024-11-27 20:29:35 -0700

    add more nouns; shuffle nouns & limit to 5

--- index.js
+++ index.js
@@ -9,23 +9,48 @@ checkbox.addEventListener('change', function () {
 });
 
 const quizData = {
+    "questionCount": 5,
     "words": [
         ["kat", "cat", "de"],
         ["appel", "apple", "de"],
-        ["huis", "house", "het"]
+        ["huis", "house", "het"],
+        ["hond", "dog", "de"],
+        ["kamer", "room", "de"],
+        ["snertweer", "awful weather", "het"],
+        ["flodder", "mess", "de"],
+        ["prutswerk", "crappy job", "het"],
+        ["rommelkont", "messy person", "de"],
+        ["neushoorn", "rhino", "de"],
+        ["hart", "heart", "het"],
+        ["water", "water", "het"],
+        ["straat", "street", "de"],
+        ["auto", "car", "de"],
+        ["strand", "beach", "het"],
+        ["vrouw", "woman", "de"],
+        ["muis", "mouse", "de"],
+        ["banaan", "banana", "de"],
     ]
 };
 
 const quizContainer = document.getElementById('quizContainer');
 
 function makeQuiz(data) {
+    // shuffle, thanks Laurens Holst & Luca C. on stack overflow :3
+    for (let i = quizData["words"].length - 1; i >= 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [quizData["words"][i], quizData["words"][j]] = [quizData["words"][j], quizData["words"][i]];
+    }
+
     const allChoices = ["de", "het"];
     data.words.forEach((wordSet, index) => {
+        if (index >= quizData.questionCount) {
+            return;
+        }
         const questionDiv = document.createElement('div');
         questionDiv.className = "question";
 
         const questionText = document.createElement('h3');
-        questionText.textContent = `What is the article for "${wordSet[0]}"?`;
+        questionText.textContent = `What is the article for "${wordSet[0]}" (${wordSet[1]})?`;
         questionDiv.appendChild(questionText);
 
         allChoices.forEach((choice) => {
